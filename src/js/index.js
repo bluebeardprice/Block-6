531ae8a577b7c95425de21d1bee4d7de7dce92cd
Viktoria1604 <152924130+Viktoria1604@users.noreply.github.com>
2024-02-11 15:46:16 +0300

    block 6

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import '../scss/style.scss'
+
+import './swiper'
+import './brands-section-more-btn'
+import './about-us-more-btn'
+import './burger-menu'
+import './repair-section-more-btn'
+import './modal-page'
